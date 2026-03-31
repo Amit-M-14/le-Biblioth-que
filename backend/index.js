e24e0906a7622638f07a5cf93c6bf5ce89cab24c
Amit <amitmohantyxiv@gmail.com>
2026-03-31 21:16:29 +0530

    all the routing is completed

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,29 @@
 import express, { request } from "express";
-import { PORT,MongoDBURL } from "./config.js";
+import { PORT, MongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookmodels.js";
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
+// middleware for parsing the request body
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.status(234).send('Welcome to my page');
+app.get("/", (req, res) => {
+  res.status(234).send("Welcome to my page");
 });
-// Route to save a new book into database
 
-app.post('/book',async (req,res)=>{
-    try{
-        if(!req.body.title || !req.body.author || !req.body.publishYear){
-            return res.status(400).send({message:'send all required fields : title, author, publishYear',});
-        }
-        const newBook = {
-            title : req.body.title,
-            author : req.body.author,
-            publishYear : req.body.publishYear,
-        };
 
-        const book = await Book.create(newBook);
-
-        res.status(201).send(book);
-    }catch(error){
-        console.log(error.message);
-        res.status(500).send({message : error.message});
-    }
-})
+app.use('/book', booksRoute);
 
 mongoose
-    .connect(MongoDBURL)
-    .then(()=>{
-        console.log('App connected to database.');
-        app.listen(PORT , ()=>{
-            console.log(`The app is listening ${PORT}`);
-        })   
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
+  .connect(MongoDBURL)
+  .then(() => {
+    console.log("App connected to database.");
+    app.listen(PORT, () => {
+      console.log(`The app is listening ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
